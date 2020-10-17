fd6bc38d40a1abffd2e701364e2d90c0b02934a7
Fan Yang <fan@FandeMBP.home>
2020-10-17 16:17:48 +0300

    Check if user already login

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -6,11 +6,30 @@
 const router = require('koa-router')()
 
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
+
+/**
+ * Get login info
+ * @param {Object} ctx ctx
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false, // Default not login
+  }
+
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName,
+    }
+  }
+  return data
+}
 
 module.exports = router
